0c6c12e34a0cc58c1519717b5b90bdbff79f2486
Azie Melza Pratama <aziemp66@gmail.com>
2024-07-22 21:37:33 +0700

    feat : learn secondary constructor

---
+++ src/data/secondary_constructor/Person.kt
@@ -0,0 +1,20 @@
+package data.secondary_constructor
+
+class Person(
+  firstNameParam: String,
+  middleNameParam: String?,
+  lastNameParam: String
+) {
+  constructor(firstNameParam: String, lastNameParam: String) : this(firstNameParam, null, lastNameParam) {
+
+  }
+
+  var firstName: String = firstNameParam
+  var middleName: String? = middleNameParam
+  val lastname: String = lastNameParam
+  val  greetings : String = "Hello from $firstName $middleName $lastname"
+
+  init {
+    println(greetings)
+  }
+}
---
+++ src/learn/SecondaryConstructor.kt
@@ -0,0 +1,11 @@
+package learn
+
+import data.secondary_constructor.Person
+
+fun main() {
+  val azie = Person("Azie", "Pratama")
+  val melza = Person("Azie", "Melza", "Pratama")
+
+  println(azie)
+  println(melza)
+}

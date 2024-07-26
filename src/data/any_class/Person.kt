ee6032f7cace3accc7ea72b84534d1514774f8ad
Azie Melza Pratama <aziemp66@gmail.com>
2024-07-27 02:00:00 +0700

    feat : learn Any Class function

--- src/data/any_class/Person.kt
+++ src/data/any_class/Person.kt
@@ -1,9 +1,23 @@
 package data.any_class
 
 class Person(
-  val name :String
+  val name: String
 ) {
   override fun toString(): String {
     return "this is $name"
+  }
+
+  override fun equals(other: Any?): Boolean {
+    return when (other) {
+      is Person ->
+        this.name == other.name
+
+      else ->
+        super.equals(other)
+    }
+  }
+
+  override fun hashCode(): Int {
+    return name.hashCode()
   }
 }

d8cc4d5a99344865bca49ea364def178a5490d21
Azie Melza Pratama <aziemp66@gmail.com>
2024-07-26 22:42:10 +0700

    feat : learn properties overriding final keyword

--- src/data/overriding/Employee.kt
+++ src/data/overriding/Employee.kt
@@ -1,7 +1,7 @@
 package data.overriding
 
 open class Employee (
-  val name: String
+  open val name: String
 ) {
   open fun sayHello(name: String){
     println("Hello $name, my name is ${this.name}")
--- src/data/overriding/Manager.kt
+++ src/data/overriding/Manager.kt
@@ -1,6 +1,8 @@
 package data.overriding
 
 final class Manager(name : String) : Employee(name){
+  final override val name: String
+    get() = "Manager ${super.name}"
   final override fun sayHello(name: String) {
     println("Hello $name, my name is manager ${this.name}")
   }
